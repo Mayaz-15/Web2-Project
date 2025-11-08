9730ae758584079bc522c85aedb66ef1f5a31caf
Aryam218 <aryaam871@gmail.com>
2025-11-08 12:32:06 +0300

    Update login.php

--- LearnIT/Web2Project/login.php
+++ LearnIT/Web2Project/login.php
@@ -186,6 +186,7 @@ margin: 40px 1%;
   <input type="email" name="email" placeholder="Email" required><br>
   <input type="password" name="password" placeholder="Password" required><br>
 
+  
   <button type="submit" class="btn">Log in</button>
 
   
